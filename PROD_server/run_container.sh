cca2e74e5f8fdd3b7bcc8117de1448942f3167aa
Cesar Cueva Mendoza <cesar.cueva@bbva.com>
2019-04-04 12:08:41 -0500

    cambips

--- PROD_server/run_container.sh
+++ PROD_server/run_container.sh
@@ -3,4 +3,4 @@ docker container rm tomcat8-5-prod
 docker image rm cecum/tomcat8-5-prod_img
 docker build -t cecum/tomcat8-5-prod_img .
 #docker run -d --name "tomcat8-5-prod" -p 8090:8080  -it "cecum/tomcat8-5-prod_img"
-docker run -p 8091:8080 -it -d --name tomcat8-5-prod cecum/tomcat8-5-prod_img
+docker run -p 8090:8080 -it -d --name tomcat8-5-prod cecum/tomcat8-5-prod_img
