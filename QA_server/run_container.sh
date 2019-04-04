eee8bcb05264fe863cc733f9ff3a9bef26643e43
Cesar Cueva Mendoza <cesar.cueva@bbva.com>
2019-04-04 12:06:24 -0500

    cambios

--- QA_server/run_container.sh
+++ QA_server/run_container.sh
@@ -1,2 +1,6 @@
-docker build -t cecum/tomcat8-5-qa .
-docker run -d -it -p 8091:8080 "cecum/tomcat8-5-qa"
+docker stop tomcat8-5-qa
+docker container rm tomcat8-5-qa
+docker image rm cecum/tomcat8-5-qa_img
+docker build -t cecum/tomcat8-5-qa_img .
+#docker run -d --name "tomcat8-5-qa" -p 8090:8080  -it "cecum/tomcat8-5-qa_img"
+docker run -p 8091:8080 -it -d --name tomcat8-5-qa cecum/tomcat8-5-qa_img
